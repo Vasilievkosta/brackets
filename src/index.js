3f651cf48391b9c57831ac53af9f0c1d5052dbcd
Konstantin <vasilievkosta@gmail.com>
2020-10-21 00:57:26 +0300

    refactor: modified src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let steck = [];
+		let s;
+		let steckMini = [];
+		for (let i = 0; i < str.length; i++) {
+			
+			steck.push(str[i]);
+			steckMini = steck.slice(steck.length - 2);
+			
+			for (let i = 0; i < bracketsConfig.length; i++){
+				
+				if(JSON.stringify(steckMini)==JSON.stringify(bracketsConfig[i])){
+					
+					steck.pop();
+					steck.pop();					
+				}
+			}			
+		}		
+		return (steck.length === 0);
 }
